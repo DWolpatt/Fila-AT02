dfdd41d5a26f9c5e830138aebe04a75957342a4a
DWolpatt <deborawolpatt@gmail.com>
2021-09-30 20:11:01 -0300

    Filas Exemplo

---
+++ Fila/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fila
+{
+    class Program
+    {
+        static void Main()
+        {
+            Queue<string> clientes = new Queue<string>();
+            clientes.Enqueue("Cliente 1");
+            clientes.Enqueue("Cliente 2");
+            clientes.Enqueue("Cliente 3");
+            clientes.Enqueue("Cliente 4");
+            clientes.Enqueue("Cliente 5");
+
+            foreach(string cliente in clientes) //Percorrendo a lista.
+            {
+                Console.WriteLine(cliente);
+            }
+
+            clientes.Dequeue(); //Removendo item da fila, no conceito de fila, onde primeiro a entrar é primeiro a sair.
+
+
+            foreach (string cliente in clientes) //Percorrendo a lista novamente para analisar a saída do primeiro.
+            {
+                Console.WriteLine(cliente);
+            }
+
+        }
+    }
+}
